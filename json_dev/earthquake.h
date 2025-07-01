66c6ac3885d109ad575def2462a88c1f29357e0d
Chris Bourke <cbourke@cse.unl.edu>
2025-07-01 17:56:03 +0000

    Tue, Jul 1, 2025, 12:56 PM -05:00

--- json_dev/earthquake.h
+++ json_dev/earthquake.h
@@ -1,11 +1,13 @@
 
 
 typedef struct {
+  char *id;
   char *locationName;
   double magnitude;
   int timestamp;
   double latitude;
   double longitude;
-
-
+  int depth; //km
 } EarthquakeData
+
+
