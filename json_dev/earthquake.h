7a8a904476166f1f33ff47bb03885ce3c45773e9
Chris Bourke <cbourke@cse.unl.edu>
2025-07-01 17:59:06 +0000

    Tue, Jul 1, 2025, 12:59 PM -05:00

--- json_dev/earthquake.h
+++ json_dev/earthquake.h
@@ -10,4 +10,9 @@ typedef struct {
   int depth; //km
 } EarthquakeData
 
+const char *usgs_earthquake_url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson";
 
+/**
+ *
+ */
+EarthquakeData *loadData(int *n);
