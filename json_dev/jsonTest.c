a4a569a582e351dce94505dd8491f4238404a6a9
Chris Bourke <cbourke@cse.unl.edu>
2025-07-01 17:48:02 +0000

    Tue, Jul 1, 2025, 12:48 PM -05:00

---
+++ json_dev/jsonTest.c
@@ -0,0 +1,88 @@
+/*
+ * A simple example of json string parsing with json-c.
+ *
+ * clang -Wall -g -I/usr/include/json-c/ -o json_parser json_parser.c -ljson-c
+ */
+#include <json.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "curl_utils.h"
+
+int main() {
+
+	char *url = "https://cse.unl.edu/~cbourke/example.json";
+	char *str = getContent(url);
+
+	struct json_object *object;
+	object = json_tokener_parse(str);
+
+	printf("jobj from str:\n---\n%s\n---\n", json_object_to_json_string_ext(object, JSON_C_TO_STRING_SPACED | JSON_C_TO_STRING_PRETTY));
+
+	char *val_type_str;
+	int val_type, i;
+
+	// key and val don't exist outside of this bloc
+	json_object_object_foreach(object, key, val) {
+		printf("key: \"%s\", type of val: ", key);
+		val_type = json_object_get_type(val);
+
+		switch (val_type) {
+			case json_type_null:
+				val_type_str = "val is NULL";
+				break;
+
+			case json_type_boolean:
+				val_type_str = "val is a boolean";
+				break;
+
+			case json_type_double:
+				val_type_str = "val is a double";
+				break;
+
+			case json_type_int:
+				val_type_str = "val is an integer";
+				break;
+
+			case json_type_string:
+				val_type_str = "val is a string";
+				str = (char *) json_object_get_string(val);
+				break;
+
+			case json_type_object:
+				val_type_str = "val is an object";
+				break;
+
+			case json_type_array:
+				val_type_str = "val is an array";
+				break;
+		}
+
+		printf("%s", val_type_str);
+
+		if (str)
+			printf("\t->\t\"%s\"", str);
+
+		printf("\n");
+		str = NULL;
+	}
+
+	//get a subobject:
+	// Get the json_object associated to the "student" key
+	struct json_object *student, *tmp;
+	json_object_object_get_ex(object, "student", &student);
+
+	json_object_object_foreach(student, key2, val2) {
+		str = (char *) json_object_get_string(val2);
+		printf("key: \"%s\" -> %s \n", key2, str);
+
+		if(strcmp(key2, "id") == 0) {
+			//nope, val2 is a macro and a json_object*:
+			// int x = atoi(val2);
+			int y = json_object_get_int(val2);
+			printf("y = %d\n", y);
+		}
+	}
+
+	return 0;
+}
---
+++ json_dev/readme.md
@@ -0,0 +1,36 @@
+
+Documentation:
+
+https://json-c.github.io/json-c/json-c-0.10/doc/html/json__object_8h.html#ac11730ad909d1f9eb077d1ce9ff8b153
+
+Examples:
+https://gist.github.com/alan-mushi/19546a0e2c6bd4e059fd
+
+Install:
+
+`sudo apt install libjson-c-dev`
+
+Comppile:
+
+`gcc -Wall -g -I/usr/include/json-c/ -o json_parser json_parser.c -ljson-c`
+
+test:
+
+`https://cse.unl.edu/~cbourke/example.json`
+
+
+Potential Data:
+
+https://mixedanalytics.com/blog/list-actually-free-open-no-auth-needed-apis/
+
+https://puppygifs.tumblr.com/api/read/json
+tumblr only provides "pure" json data through registered OATH, likely throttled
+
+Chicago Institute of Art:
+https://api.artic.edu/docs/#quick-start
+More of an API, but has a full data dump to avoid pagination/throttling
+
+
+https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson
+
+https://earthquake.usgs.gov/earthquakes/feed/v1.0/geojson.php
